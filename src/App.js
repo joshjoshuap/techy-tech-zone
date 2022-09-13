e7dc0df40f77eddc0141be29150ac4ff44e3a49a
Joshua <joshjoshuap1@gmail.com>
2022-09-13 17:53:02 +0800

    chore: add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return (
-    <div className="App">
-      <h1> Hello </h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <div className="container">
+        <a className="navbar-brand fs-3" href="/">
+          TechyTech
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav fs-5 fw-semibold">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Laptop
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Phone
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Tablet
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Gadgets
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
