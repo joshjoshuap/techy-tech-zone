43f7b7ad9c9dc3af87b2a0cf1ba4e853d2426f82
Joshua <joshjoshuap1@gmail.com>
2022-09-14 17:15:25 +0800

    chore: initialize home sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import Header from "../sections/Header";
+import Section from "../components/Section";
+import sampleLaptop from "../assets/images/sample-laptop.jpg";
 import Main from "../sections/Main";
 
 const Home = () => {
