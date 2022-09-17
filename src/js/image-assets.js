b385bdfd65cceebec2fbe48539c537bc8b1a921c
Joshua <joshjoshuap1@gmail.com>
2022-09-17 18:02:11 +0800

    chore: add items in laptop

--- src/js/image-assets.js
+++ src/js/image-assets.js
@@ -12,7 +12,21 @@ import lenovoLogo from "../assets/images/logo/lenovo-logo.png";
 import dellLogo from "../assets/images/logo/dell-logo.png";
 import hpLogo from "../assets/images/logo/hp-logo.png";
 
-// Image
+// Laptop Image
+import lenovoLegion5 from "../assets/images/laptop/lenovo-legion5.jpg";
+import lenovoX1Carbon from "../assets/images/laptop/lenovo-x1carbon.jpg";
+import lenovoYoga6 from "../assets/images/laptop/lenovo-yoga6.jpg";
+import lenovoIdepad3 from "../assets/images/laptop/lenovo-idepad3.jpg";
+import acerPredatorHelio from "../assets/images/laptop/acer-predatorhelios.jpg";
+import acerNitro5 from "../assets/images/laptop/acer-nitro5.jpg";
+import acerSwift5 from "../assets/images/laptop/acer-swift5.jpg";
+import acerAspire7 from "../assets/images/laptop/acer-aspire7.jpg";
+import asusZenbook14 from "../assets/images/laptop/asus-zenbook14.jpg";
+import asusVivobookPro14 from "../assets/images/laptop/asus-vivobookpro14.jpg";
+import dellXPS13 from "../assets/images/laptop/dell-xps13.jpg";
+import dellInsipiron15 from "../assets/images/laptop/dell-inpiron15.jpg";
+import hpSpectreX360 from "../assets/images/laptop/hp-spectrex360.jpg";
+import hpEnvyX360 from "../assets/images/laptop/hp-envyx360.jpg";
 import sampleLaptop from "../assets/images/sample-laptop.jpg";
 import samplePhone from "../assets/images/sample-phone.jpg";
 import sampleTablet from "../assets/images/sample-tablet.jpg";
@@ -29,6 +43,20 @@ export {
   lenovoLogo,
   dellLogo,
   hpLogo,
+  lenovoLegion5,
+  lenovoX1Carbon,
+  lenovoYoga6,
+  lenovoIdepad3,
+  acerPredatorHelio,
+  acerNitro5,
+  acerSwift5,
+  acerAspire7,
+  asusZenbook14,
+  asusVivobookPro14,
+  dellXPS13,
+  dellInsipiron15,
+  hpSpectreX360,
+  hpEnvyX360,
   sampleLaptop,
   samplePhone,
   sampleTablet,
