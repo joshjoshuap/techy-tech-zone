2bf30fd4ffacada240e800dcf88246726f2309aa
Joshua <joshjoshuap1@gmail.com>
2022-09-20 15:21:18 +0800

    chore: responsive, mobile support

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -4,18 +4,18 @@ const specList = (product) => {
 
 const HomeSection = (props) => {
   return (
-    <div className="py-5">
+    <div className="mb-5">
       <h1 className="display-2">{props.sectionTitle}</h1>
       <div className="row py-3">
-        <div className="col text-center">
+        <div className="col-12 col-md align-self-center text-center">
           <img
-            className="img-fluid rounded-2"
+            className="img-fluid rounded-2 image-responsive"
             src={props.imageProduct1}
             alt="Laptop Iamge"
           ></img>
         </div>
-        <div className="col px-5">
-          <h1 className="mb-4 text-decoration-underline">
+        <div className="col-12 col-lg px-lg-5 mt-4 mt-lg-1">
+          <h1 className="mb-2 mb-lg-4 text-decoration-underline text-center">
             {props.titleProduct1}
           </h1>
           <p>{props.descProduct1}</p>
@@ -23,18 +23,18 @@ const HomeSection = (props) => {
           <ul className="lh-lg">{props.specsProduct1.map(specList)}</ul>
         </div>
       </div>
-      <div className="row py-5">
-        <div className="col px-5">
-          <h1 className="mb-4 text-decoration-underline">
+      <div className="row py-3">
+        <div className="col-12 col-lg  px-lg-5 mt-4 mt-lg-1 order-2 order-lg-1">
+          <h1 className="mb-2 mb-lg-4 text-decoration-underline text-center">
             {props.titleProduct2}
           </h1>
           <p>{props.descProduct2}</p>
           <h4>Specifications</h4>
           <ul className="lh-lg">{props.specsProduct2.map(specList)}</ul>
         </div>
-        <div className="col text-center">
+        <div className="col-12 col-lg align-self-center text-center order-1 order-lg-2">
           <img
-            className="img-fluid rounded-2"
+            className="img-fluid rounded-2 image-responsive"
             src={props.imageProduct2}
             alt="Laptop Iamge"
           ></img>
--- src/components/ItemSection.js
+++ src/components/ItemSection.js
@@ -1,7 +1,7 @@
 const Section = (props) => {
   return (
-    <div className="py-5">
-      <h1 className="display-2">{props.sectionTitle}</h1>
+    <div className="mb-5">
+      <h1 className="display-2 mb-3">{props.sectionTitle}</h1>
       <div className="d-flex flex-wrap gap-3 justify-content-center">
         {props.children}
       </div>
